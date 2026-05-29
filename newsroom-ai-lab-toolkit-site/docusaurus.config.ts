4e80d6e78c09c4a060104b37ab0748019369ccb9
Paige <moodypaige.e@gmail.com>
2026-05-29 11:43:13 -0400

    try again

--- newsroom-ai-lab-toolkit-site/docusaurus.config.ts
+++ newsroom-ai-lab-toolkit-site/docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://paigemoody.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/newsroom-ai-lab-toolkit/',
+  baseUrl: '/newsroom-ai-lab-toolkit.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'paigemoody', // Usually your GitHub org/user name.
-  projectName: 'newsroom-ai-lab-toolkit', // Usually your repo name.
+  projectName: 'newsroom-ai-lab-toolkit.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   deploymentBranch: 'main',
